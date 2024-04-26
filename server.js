d33e8304465e19ea8c3c387d27e14d1f65ab7930
Seb <63566437+sebcopixl@users.noreply.github.com>
2024-04-26 15:34:16 -0400

    search criteria set succesfuly in server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 
 // Enable CORS middleware
 app.use(cors());
-
 // MySQL database connection configuration
 const connection = mysql.createConnection({
   host: "localhost",
@@ -22,17 +21,23 @@ connection.connect((err) => {
   console.log("Connected to MySQL database");
 });
 
-// API endpoint to retrieve all plants
+// API endpoint to retrieve plants matching the search term
 app.get("/plants", (req, res) => {
-  // Query the database to retrieve all plant data
-  connection.query("SELECT * FROM plants", (err, results) => {
-    if (err) {
-      console.error("Error retrieving plants from database: " + err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
+  const searchTerm = req.query.search;
+
+  // Query the database to retrieve plants matching the search term
+  connection.query(
+    "SELECT * FROM plants WHERE common_name LIKE ? OR scientific_name LIKE ?",
+    [`%${searchTerm}%`, `%${searchTerm}%`],
+    (err, results) => {
+      if (err) {
+        console.error("Error retrieving plants from database: " + err);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
+      }
+      res.json(results); // Send plant data as JSON response
     }
-    res.json(results); // Send plant data as JSON response
-  });
+  );
 });
 
 const PORT = process.env.PORT || 3000;
