a91ccc337b4efa0053b28bdf72bfb7507115f4bf
Sebastian Correa <63566437+sebcopixl@users.noreply.github.com>
2024-05-07 09:10:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: "3306",
   user: "root",
-  password: "Northface71217/",
+  password: "N/",
   database: "herbarium",
 });
 
